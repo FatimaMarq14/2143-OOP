76fa83e9e39c4086d60d88a41dbe36129874632f
Fatima Marquez <123118716+FatimaMarq14@users.noreply.github.com>
2024-09-20 08:48:23 -0500

    Update main.cpp

--- Assignments/P01/main.cpp
+++ Assignments/P01/main.cpp
@@ -9,8 +9,11 @@
  *
  *  Description:
  *        This program implements a class called Fraction that includes
- *         overloaded operators ( + - * / ) to do between fractions. It
- *         uses GCD, LCD, and LCM to simplify the fractions.
+ *        overloaded operators ( + - * / ) to do between fractions. It
+ *        uses GCD, LCD, and LCM to simplify the fractions and also shows
+ *        how private and public methods are implemented. It also includes
+ *        a parse function that I learned outside of class to help convert
+ *        my string into fractions.
  *  Code Help:
  *  https://stackoverflow.com/questions/40009563/
  *  how-to-split-a-fraction-into-two-integers-c
